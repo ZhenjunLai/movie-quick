d2a835cab1d975b0bbcbdba7f47392935950aff9
laizhenjun <laizhenjun@skyworth.com>
2022-07-29 17:52:06 +0800

    引入react-router-dom6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from '../src/pages/home/index'
 function App() {
   return (
     <div className="App">
-      <h1> this is react program</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   )
 }
